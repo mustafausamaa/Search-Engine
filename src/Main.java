e09b929b2f56ccd6ecb0696f65c8e4f8544becad
abdallah808 <abdallahahmedfawzy@outlook.com>
2022-05-19 02:19:11 +0200

    multi-threaded-WebCrawler

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,5 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
-        ArrayList<Thread>threads=new ArrayList<Thread>();
-        for(int i=0;i<20;i++) {
-            WebCrawler crawlingThread0 = new WebCrawler(i);
-            Thread thread = new Thread(crawlingThread0);
-            thread.start();
-            threads.add(thread);
-        }
-        for(int i=0;i<20;i++) {
-           threads.get(i).join();
-        }
-        //visitedLinks.txt contains all the urls
-        System.out.print("Finished crawling :\n");
 
-    }
 }
