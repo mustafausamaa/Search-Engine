cdf62fe3bf9a7d0afcfe4f890ef0ba7002efb115
mehrez1110 <77018385+mehrez1110@users.noreply.github.com>
2022-05-19 03:59:44 +0200

    frontend

---
+++ frontend/search_engine_project/lib/Documens-controller.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:momentum/momentum.dart';
+import 'package:search_engine_project/Documents-model.dart';
+import 'package:search_engine_project/link-component.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// This is a controller for a view and a model.
+// Keep all your business logic here.
+// Trigger your API calls here and set the data in the ViewModels from here as well.
+// Add .controller suffix to denote association with a model and a view.
+
+class DocumentsController extends MomentumController<DocumentsModel> {
+  @override
+  DocumentsModel init() {
+    return DocumentsModel(this, searchDocuments: [], isLoading: false);
+  }
+
+  Future<void> getSearchDocuments(searchInput, context) async {
+    model.update(isLoading: true);
+    var url = Uri.https(
+        "localhost:8080", "/documents", {"searchWord": "searchInput"});
+    var response = await http.get(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+    );
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      print(response.body);
+      List Documents = [];
+      model.update(searchDocuments: Documents);
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      for (var responseItem in jsonResponse['results']) {
+        print(responseItem["name"]);
+        final field = LinkViewComponent(
+          url: responseItem["url"],
+          description: responseItem["description"],
+          title: responseItem['title'],
+        );
+        Documents.add(field);
+      }
+      model.update(searchDocuments: Documents);
+      model.update(isLoading: false);
+    } else
+      print("error");
+  }
+}
---
+++ frontend/search_engine_project/lib/link-component.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:search_engine_project/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkViewComponent extends StatelessWidget {
+  final url;
+  final title;
+  final description;
+  const LinkViewComponent({Key key, this.url, this.title, this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+        color: tertiary,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 229, 229, 230),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, -5),
+          )
+        ],
+      ),
+      padding: EdgeInsets.all(10),
+      height: 80,
+      child: RichText(
+          // maxLines: 2,
+          text: TextSpan(children: [
+        TextSpan(
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              launch(url.toString());
+            },
+          text: title.toString(),
+          style: TextStyle(
+            color: primary,
+            decoration: TextDecoration.underline,
+            fontWeight: FontWeight.bold,
+            fontSize: (20),
+          ),
+        ),
+        TextSpan(
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              launch(url.toString());
+            },
+          text: description.toString(),
+          style: TextStyle(
+            height: 1.5,
+            color: secondary,
+            fontWeight: FontWeight.w500,
+            fontSize: (14),
+          ),
+        )
+      ])),
+    );
+  }
+}
